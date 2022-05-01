d8f157975cc990d06049381d1a08373a43434a77
Nils <scni1081@h-ka.de>
2022-05-01 16:44:34 +0200

    Activity Einstellungen + Logo + Header Update

--- app/src/main/java/eu/iums/fitwork/SettingsActivity.java
+++ app/src/main/java/eu/iums/fitwork/SettingsActivity.java
@@ -1,14 +1,18 @@
 package eu.iums.fitwork;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
     }
 }
