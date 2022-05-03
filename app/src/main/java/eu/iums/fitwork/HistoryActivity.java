116560bdd59e3017264bda47e58f629c08042733
mach1031 <mach1031@h-ka.de>
2022-05-03 19:57:53 +0200

    ListViews Leaderboard + Verlauf

--- app/src/main/java/eu/iums/fitwork/HistoryActivity.java
+++ app/src/main/java/eu/iums/fitwork/HistoryActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class HistoryActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class HistoryActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     Toolbar toolbar;
 
@@ -19,5 +26,30 @@ public class HistoryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Verlauf");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ListView history_list = (ListView) findViewById(R.id.listView);
+
+        String[] modes = new String[] {"Fuß", "Fahrrad", "ÖPNV", "E-Scooter", "MIV-Fahrer", "MIV-Mitfahrer", "Sonstiges"};
+        ArrayList<String> modesList = new ArrayList<>();
+        modesList.addAll(Arrays.asList(modes));
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.listview, R.id.textView, modesList);
+
+        history_list.setAdapter(listAdapter);
+
+        history_list.setOnItemClickListener(this);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    /*@Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        selectedMode = parent.getItemAtPosition(position).toString();
+        Intent setMode = new Intent(TransportationMode.this, MainActivity.class);
+        setMode.putExtra(KEY, selectedMode);
+        startActivity(setMode);
+    }*/
 }
--- app/src/main/java/eu/iums/fitwork/LeaderboardActivity.java
+++ app/src/main/java/eu/iums/fitwork/LeaderboardActivity.java
@@ -3,9 +3,17 @@ package eu.iums.fitwork;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class LeaderboardActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeaderboardActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     Toolbar toolbar;
 
@@ -19,5 +27,30 @@ public class LeaderboardActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Leaderboard");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ListView ranks_list = (ListView) findViewById(R.id.listView);
+
+        String[] modes = new String[] {"Fuß", "Fahrrad", "ÖPNV", "E-Scooter", "MIV-Fahrer", "MIV-Mitfahrer", "Sonstiges"};
+        ArrayList<String> modesList = new ArrayList<>();
+        modesList.addAll(Arrays.asList(modes));
+
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.listview, R.id.textView, modesList);
+
+        ranks_list.setAdapter(listAdapter);
+
+        ranks_list.setOnItemClickListener(this);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    /*@Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        selectedMode = parent.getItemAtPosition(position).toString();
+        Intent setMode = new Intent(TransportationMode.this, MainActivity.class);
+        setMode.putExtra(KEY, selectedMode);
+        startActivity(setMode);
+    }*/
 }
