36a23db368a3a86e560033444af1bba5e4bf9a3b
mach1031 <mach1031@h-ka.de>
2022-05-05 00:45:32 +0200

    Layout Main Update, Layout Register Update, Layout Settings Update, Time Picker, Pausenalarm noch nicht funktionstüchtig

--- app/src/main/java/eu/iums/fitwork/SettingsActivity.java
+++ app/src/main/java/eu/iums/fitwork/SettingsActivity.java
@@ -3,16 +3,46 @@ package eu.iums.fitwork;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class SettingsActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class SettingsActivity extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
 
     Toolbar toolbar;
+    Button select_time;
+    TextView selected_time;
+    String time;
+    Switch switch_alert;
+    boolean alarm_state;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String SELECTED_TIME = "time";
+    public static final String SWITCH_ALARM = "switch_alarm";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        selected_time = (TextView) findViewById(R.id.selected_time);
 
         //Toolbar
         toolbar = findViewById(R.id.toolbar);
@@ -20,5 +50,82 @@ public class SettingsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Einstellungen");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        select_time = (Button) findViewById(R.id.select_time);
+        select_time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getFragmentManager(), "time picker");
+                saveData();
+            }
+        });
+
+        switch_alert = (Switch) findViewById(R.id.switch_alert);
+        switch_alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+        if(switch_alert.isChecked()) {
+            Calendar c = Calendar.getInstance();
+            startAlarm(c);
+        }
+
+        loadData();
+        updateViews();
+
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        updateTimeText(c);
+    }
+
+    private void updateTimeText(Calendar c) {
+        String timeText;
+        timeText = DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+
+        selected_time.setText(timeText);
+        saveData();
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(SELECTED_TIME, selected_time.getText().toString());
+        editor.putBoolean(SWITCH_ALARM, switch_alert.isChecked());
+
+        editor.apply();
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+        time = sharedPreferences.getString(SELECTED_TIME, "");
+        alarm_state = sharedPreferences.getBoolean(SWITCH_ALARM, false);
+
+    }
+
+    public void updateViews() {
+        selected_time.setText(time);
+        switch_alert.setChecked(alarm_state);
+    }
+
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
     }
 }
